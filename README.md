d9fe7b61316266b4d5571281e451cc8ef1ac5c58
ruoyu <hunger@jirengu.com>
2017-12-13 11:35:11 +0800

    finish

--- README.md
+++ README.md
@@ -1,2 +1,35 @@
-# xiedaimala-upload
-xiedaimala.com upload video
+# 写代码啦视频上传工具
+
+## 功能
+- 登录验证，xiedaimal.com 具有老师角色的用户可登录上传
+- 上传视频到服务器，展示上传后的线上资源链接
+- 支持增量上传
+- 支持断点续传
+- 可查看自己的上传历史记录
+- 管理员可查看所有用户上传历史记录
+
+
+## 安装
+
+```bash
+npm install -g xiedaimala
+```
+
+## 使用范例
+
+- `xdml upload`   增量上传当前目录下所有的视频和图片到服务器，如果之前上传过不会再次上传
+- `xdml history`  查看当前用户上传后的线上预览链接历史记录，默认展示24小时内的上传历史
+- `xdml history -d 3`  默认展示3天内的上传历史
+- `xdml history -d 3 -a`  如果是管理员，则展示所有用户上传的近3天的历史记录
+- `xdml history -u hunger@qq.com`  如果是管理员，则展示该用户的历史记录
+- `xdml login`  使用xiedaimala.com 账号登录，登录信息默认保存7天
+- `xdml set -d /Users/hugner/videos`  设置默认上传路径，如果不设置则为当前命令行路径
+- `xdml clear`  清空本地历史记录，清空后用户再次执行上传会上传指定目录的全部文件
+- `xdml help`  查看具体使用信息
+
+
+by 饥人谷
+
+
+
+
--- bin/xiedaimala.js
+++ bin/xiedaimala.js
@@ -1,55 +1,99 @@
 #! /usr/bin/env node
 
-var _ = require('lodash');
-var program = require('commander');
-var colors = require('colors');
-
-
-
-
-
+var _ = require('lodash')
+var program = require('commander')
+var colors = require('colors')
 var auth = require('../lib/auth')
 var video = require('../lib/video')
+var config = require('../lib/config')
+var pkg = require('../package.json')
+var version = require('latest-version');
 
-var pkg = require('../package.json');
+version(pkg.name).then(function(version) {
+  if(version !== pkg.version){
+    console.log(`Latest version is ${colors.green(version)}, but your version is too old (${colors.grey(pkg.version)}) `)
+    console.log(`Run ${colors.green(`npm install -g ${pkg.name}`)} to update your cli, before use it`)
+  }else{
+    bootstrap()
+  }
+}).catch(function(){
+  bootstrap()
+})
 
-program
-    .command('upload')
-    .description('Upload all the videos in the current folder')
-    .action(function(){
-        video.upload()
-    });
 
-program
-    .command('login')
-    .description('Login xiedaimala.com account')
-    .action(function(){
-      auth.inputLogin().then(function(){
-      }).catch(function(){
-        console.log('login fail')
+function bootstrap(){
+  program.version(pkg.version)
+
+  program
+      .command('upload')
+      .description('Upload all the videos in the current folder')
+      .action(function(){
+          video.upload()
+      });
+
+  program
+      .command('login')
+      .description('Login xiedaimala.com account')
+      .action(function(){
+        auth.inputLogin().then(function(){
+        }).catch(function(){
+          console.log('login fail')
+        })
       })
-    })
 
 
-program
-    .command('history')
-    .description('Show uploaded history')
-    .option("-d, --days [count]", "Upload history in recent days, the default is one day")
-    .option("-a, --all ", "Show all users' upload history")
-    .action(function(options){
-      video.showHistory(options.days, options.all)
-    }).on('--help', function(){
-      console.log('  Examples:')
-      console.log()
-      console.log(colors.green('    $') + ' xdml history')
-      console.log(colors.green('    $') + ' xdml history -d 2')
-      console.log(colors.green('    $') + ' xdml history -d 10 -a')
-      console.log()
-    })
+  program
+      .command('history')
+      .description('Show uploaded history')
+      .option("-d, --days [count]", "Upload history in recent days, the default is one day")
+      .option("-a, --all ", "Show all users' upload history")
+      .option("-u, --user [email]", "Show user's upload history")
+      .action(function(options){
+        video.showHistory(options.days, options.all, options.user)
+      }).on('--help', function(){
+        console.log('  Examples:')
+        console.log()
+        console.log(colors.green('    $') + ' xdml history')
+        console.log(colors.green('    $') + ' xdml history -d 2')
+        console.log(colors.green('    $') + ' xdml history -d 10 -a')
+        console.log(colors.green('    $') + ' xdml history -u hunger')
+        console.log()
+      })
+
+  program
+      .command('set')
+      .description('set upload folder')
+      .option("-d, --dir [folderpath]", "Set the folder path as the upload directory")
+      .action(function(options){
+        if(!options.dir){
+          console.log(colors.red('Parameter error:') + ' you should use `-d folderpath` to set upload directory')
+        }else {
+          config.setUpload(options.dir)
+        }
+      }).on('--help', function(){
+        console.log('  Examples:')
+        console.log()
+        console.log(colors.green('    $') + ' xdml set -d /Users/hunger/videos/')
+        console.log()
+      })
+
+  program
+      .command('clear')
+      .description('clear local uploaded files\'  local cache')
+      .action(function(){
+        config.clearCache()
+      }).on('--help', function(){
+        console.log('  Examples:')
+        console.log()
+        console.log(colors.green('    $') + ' xdml clear')
+        console.log()
+      })
 
 
 
-// Parse and fallback to help if no args
-if(_.isEmpty(program.parse(process.argv).args) && process.argv.length === 2) {
-    program.help();
+  // Parse and fallback to help if no args
+  if(_.isEmpty(program.parse(process.argv).args) && process.argv.length === 2) {
+      program.help();
+  } 
 }
+
